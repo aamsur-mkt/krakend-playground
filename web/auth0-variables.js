078c850a6905013ddf6ff40b44772a512e50eced
Aam Surganda <surganda@gmail.com>
2019-08-16 09:42:21 +0000

    requirement changes

--- web/auth0-variables.js
+++ web/auth0-variables.js
@@ -1,5 +1,5 @@
-var AUTH0_CLIENT_ID='AUTH0_CLIENT_ID';
-var AUTH0_DOMAIN='AUTH0_DOMAIN';
-var AUTH0_AUDIENCE = 'AUTH0_AUDIENCE';
+var AUTH0_CLIENT_ID='YKcCVH7FD0NtQRXB7GnkoiF1cvRObuXE';
+var AUTH0_DOMAIN='mkt-aamsur.auth0.com';
+var AUTH0_AUDIENCE = 'https://mkt-aamsur.auth0.com/api/v2/';
 var AUTH0_CALLBACK_URL = location.href;
 
